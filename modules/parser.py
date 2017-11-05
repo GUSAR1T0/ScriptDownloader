9a86195fa5df99678c49b357a522f429442a83af
Roman Mashenkin <xromash@vxdesign.store>
2017-11-06 01:58:10 +0300

    Update method definitions
    Signed-off-by: Roman Mashenkin <xromash@vxdesign.store>

--- modules/parser.py
+++ modules/parser.py
@@ -10,12 +10,12 @@ PROTOCOL_PATTERN = re.compile(r"^https?://.*$")
 
 
 class Parser(HTMLParser):
-    def __init__(self, url):
+    def __init__(self, url: str):
         super().__init__()
         self.__url = url
         self.__scripts = dict()
 
-    def handle_starttag(self, tag, attrs):
+    def handle_starttag(self, tag: str, attrs: list) -> None:
         if tag == SCRIPT_TAG:
             link = dict(attrs).get(SOURCE_ATTRIBUTE)
             if link:
@@ -25,13 +25,13 @@ class Parser(HTMLParser):
                     else utils.parse_protocol_and_domain(self.__url) + link
                 )
 
-    def feed(self, data):
+    def feed(self, data: str) -> dict:
         HTMLParser.feed(self, data)
         return self.__scripts
 
-    def error(self, message):
+    def error(self, message: str) -> None:
         pass
 
 
-def parse(url):
+def parse(url: str) -> dict:
     return Parser(url).feed(utils.load_content(url))
--- modules/utils.py
+++ modules/utils.py
@@ -1,8 +1,8 @@
-def parse_protocol_and_domain(url: str):
+def parse_protocol_and_domain(url: str) -> str:
     from urllib import parse
     return '{uri.scheme}://{uri.netloc}'.format(uri=parse.urlparse(url))
 
 
-def load_content(url: str):
+def load_content(url: str) -> str:
     import requests
     return requests.get(url).text
