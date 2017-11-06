008537114c675f9e03366ea50f579a60b86227da
Roman Mashenkin <xromash@vxdesign.store>
2017-11-06 18:40:09 +0300

    Save scripts on disk and refactor some code parts
    Signed-off-by: Roman Mashenkin <xromash@vxdesign.store>

---
+++ modules/scripts/parser.py
@@ -0,0 +1,37 @@
+from html.parser import HTMLParser
+
+from modules.utils import get_absolute_link
+from modules.utils import load_content
+
+
+class ScriptParser(HTMLParser):
+    SCRIPT_TAG = 'script'
+    SOURCE_ATTRIBUTE = 'src'
+
+    def __init__(self, site_url: str):
+        super().__init__()
+        self.__site_url = site_url
+        self.__scripts = []
+
+    def handle_starttag(self, tag: str, attrs: list) -> None:
+        if tag == ScriptParser.SCRIPT_TAG:
+            source_link = dict(attrs).get(ScriptParser.SOURCE_ATTRIBUTE)
+            if source_link:
+                self.scripts.append(get_absolute_link(source_link, self.__site_url))
+
+    def error(self, message: str) -> None:
+        pass
+
+    @property
+    def scripts(self) -> list:
+        return self.__scripts
+
+    @staticmethod
+    def parse(site_urls: list) -> list:
+        scripts = []
+        for site_url in site_urls:
+            parser = ScriptParser(site_url)
+            data = load_content(site_url)
+            HTMLParser.feed(parser, data)
+            scripts.extend(parser.scripts)
+        return scripts
--- modules/utils.py
+++ modules/utils.py
@@ -1,8 +1,28 @@
+def load_content(url: str) -> str:
+    import requests
+    return requests.get(url).text
+
+
 def parse_protocol_and_domain(url: str) -> str:
     from urllib import parse
     return '{uri.scheme}://{uri.netloc}'.format(uri=parse.urlparse(url))
 
 
-def load_content(url: str) -> str:
-    import requests
-    return requests.get(url).text
+def parse_domain(url: str) -> str:
+    from urllib import parse
+    return '{uri.netloc}'.format(uri=parse.urlparse(url))
+
+
+def parse_filename(url: str) -> str:
+    filename = url.split('/')[-1]
+    return filename.split('?')[0]
+
+
+def get_absolute_link(source_link: str, site_url: str) -> str:
+    import re
+
+    protocol_pattern = re.compile(r'^https?://.*$')
+    if protocol_pattern.search(source_link):
+        return source_link
+    else:
+        return parse_protocol_and_domain(site_url) + ('/' if source_link[0] != '/' else '') + source_link
